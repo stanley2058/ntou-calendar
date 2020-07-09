5698de96e8fcf6d394c67ee392025690489a8d6d
stanley2058 <stanley2058@yahoo.com.tw>
2020-07-09 23:44:36 +0800

    fix port

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ const corsOptions = {
     credentials: true,
 };
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 app.use(cors(corsOptions));
 
@@ -66,4 +68,4 @@ function fetchCalendar() {
     });
 }
 
-app.listen(3000, () => console.log('Server running on port 3000!'));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}!`));
